33e025500f12e8637542c3d390139101209814ba
pdkiller666 <pdkiller666@gmail.com>
2026-04-18 00:16:31 +0700

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -10,6 +10,7 @@ import { logger } from "./lib/logger";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app: Express = express();
+app.set('trust proxy', 1);
 
 app.use(
   pinoHttp({
